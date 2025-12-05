f5268a87b8a999cd088e28ab8a0ef57766250a32
Ivy Xu <xyan21418@gmail.com>
2025-12-05 23:21:01 +0200

    day5, part2

--- day05.go
+++ day05.go
@@ -5,11 +5,11 @@ import (
     "os"
 	"strings"
 	"strconv"
+	"sort"
 )
 
 type Range struct {
-	start int
-	end   int
+	start, end int
 }
 
 func isFresh(id int, ranges []Range) bool {
@@ -60,11 +60,67 @@ func main() {
 	// fmt.Println("Ints:", ids)
 
 	// part1
-	res := 0
-	for _, id := range ids {
-		if isFresh(id, ranges) {
-			res++
+	// res := 0
+	// for _, id := range ids {
+	// 	if isFresh(id, ranges) {
+	// 		res++
+	// 	}
+	// }
+	// fmt.Println("part1:", res)
+
+	// part2
+	resultRanges := []Range{}
+	resultRanges = append(resultRanges, ranges[0])
+
+	for i := range ranges[1:] {
+		r := &ranges[i+1]
+		toAppend := true
+		toDelete := []int{}
+
+		for j := range resultRanges {
+			rr := &resultRanges[j]
+
+			// r fully covers rr
+			if r.start < rr.start && r.end > rr.end {
+				toDelete = append(toDelete, j)
+				continue
+			}
+			// left overlap
+			if r.start < rr.start && r.end >= rr.start && r.end <= rr.end {
+				r.end = rr.end
+				toDelete = append(toDelete, j)
+				continue
+			}
+			// right overlap
+			if r.start >= rr.start && r.start <= rr.end && r.end > rr.end {
+				r.start = rr.start
+				toDelete = append(toDelete, j)
+				continue
+			}
+			// r is inside rr -> no need to append
+			if r.start >= rr.start && r.end <= rr.end {
+				toAppend = false
+				break
+			}
+		}
+
+		// delete in descending order to avoid messing up indices
+		sort.Slice(toDelete, func(i, j int) bool {
+			return toDelete[i] > toDelete[j]
+		})
+		for _, idx := range toDelete {
+			resultRanges = append(resultRanges[:idx], resultRanges[idx+1:]...)
+		}
+
+		// append r to resultRanges
+		if toAppend {
+			resultRanges = append(resultRanges, *r)
 		}
 	}
-	fmt.Println("part1:", res)
+
+	res := 0
+	for _, rr := range resultRanges {
+		res += rr.end - rr.start + 1
+	}
+	fmt.Println("part2:", res)
 }
