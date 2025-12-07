597b302fa7e0c4c93b33a2e61c778caf997721f7
Ivy Xu <xyan21418@gmail.com>
2025-12-07 05:50:07 +0200

    refactoring

--- day04.go
+++ day04.go
@@ -6,6 +6,12 @@ import (
 	"strings"
 )
 
+var dirs = [][2]int{
+	{-1, -1}, {-1, 0}, {-1, 1},
+	{0, -1}, {0, 1},
+	{1, -1}, {1, 0}, {1, 1},
+}
+
 func printGrid(grid [][]rune) {
 	for _, row := range grid {
 		fmt.Println(string(row))
@@ -21,6 +27,50 @@ func copyGrid(grid [][]rune) [][]rune {
 	return newGrid
 }
 
+func solvePart1(grid [][]rune) (int, [][]rune) {
+	removeCount := 0
+	newGrid := copyGrid(grid)
+	// printGrid(grid)
+	for r := range grid {
+		for c := range grid[r] {
+			if grid[r][c] != '@' {
+				continue
+			}
+			count := 0
+			for _, d := range dirs {
+				nr := r + d[0]
+				nc := c + d[1]
+
+				if nr >= 0 && nr < len(grid) && nc >= 0 && nc < len(grid[0]) {
+					neighbor := grid[nr][nc]
+					if neighbor == '@' {
+						count++
+					}
+				}
+			}
+			if count < 4 {
+				removeCount += 1
+				newGrid[r][c] = 'x'
+			}
+		}
+	}
+	// printGrid(newGrid)
+	return removeCount, newGrid
+}
+
+func solvePart2(grid [][]rune) int {
+	removeTotal := 0
+	for {
+		removeCount, newGrid := solvePart1(grid)
+		if removeCount == 0 {
+			break
+		}
+		removeTotal += removeCount
+		grid = newGrid
+	}
+	return removeTotal
+}
+
 func main() {
     data, err := os.ReadFile("inputs/day04.txt")
     if err != nil {
@@ -31,89 +81,12 @@ func main() {
 	lines := strings.Split(string(data), "\n")
 
 	grid := make([][]rune, len(lines))
-	grid_cpy := make([][]rune, len(lines))
 	for i, line := range lines {
+		grid[i] = make([]rune, len(line))
 		grid[i] = []rune(line)
-		grid_cpy[i] = []rune(line)
 	}
 
-	printGrid(grid)
-
-	dirs := [][2]int{
-		{-1, -1}, {-1, 0}, {-1, 1},
-		{0, -1}, {0, 1},
-		{1, -1}, {1, 0}, {1, 1},
-	}
-
-	// part1
-	// res := 0
-	// for r := range grid {
-	// 	for c := range grid[r] {
-	// 		if grid[r][c] != '@' {
-	// 			continue
-	// 		}
-	// 		count := 0
-	// 		for _, d := range dirs {
-	// 			nr := r + d[0]
-	// 			nc := c + d[1]
-
-	// 			if nr >= 0 && nr < len(grid) && nc >= 0 && nc < len(grid[0]) {
-	// 				neighbor := grid[nr][nc]
-	// 				if neighbor == '@' {
-	// 					count++
-	// 				}
-	// 			}
-	// 		}
-	// 		if count < 4 {
-	// 			res += 1
-	// 			grid_cpy[r][c] = 'x'
-	// 		}
-	// 	}
-	// }
-
-	// fmt.Println("-----")
-	// printGrid(grid_cpy)
-
-	// fmt.Println("part1:", res)
-
-
-	// part2
-	removeTotal := 0
-	for {
-		removeCount := 0
-		grid_cpy = copyGrid(grid)
-		for r := range grid {
-			for c := range grid[r] {
-				if grid[r][c] != '@' {
-					continue
-				}
-				count := 0
-				for _, d := range dirs {
-					nr := r + d[0]
-					nc := c + d[1]
-
-					if nr >= 0 && nr < len(grid) && nc >= 0 && nc < len(grid[0]) {
-						neighbor := grid[nr][nc]
-						if neighbor == '@' {
-							count++
-						}
-					}
-				}
-				if count < 4 {
-					removeCount += 1
-					grid_cpy[r][c] = 'x'
-				}
-			}
-		}
-		if removeCount == 0 {
-			break
-		}
-		removeTotal += removeCount
-		grid = grid_cpy
-	}
-
-	fmt.Println("-----")
-	printGrid(grid_cpy)
-
-	fmt.Println("part2:", removeTotal)
+	part1Res, _ := solvePart1(grid)
+	fmt.Println("part1:", part1Res)
+	fmt.Println("part2:", solvePart2(grid))
 }
